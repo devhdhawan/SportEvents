d3371a7d9c3bc0f74497d3eb754d55a46090460e
Himanshu Dhawan <himanshudimp05@gmail.com>
2025-11-12 09:49:36 +0530

    Removing Archive folder

--- archive/cache/redis_value.py
+++
@@ -1,9 +0,0 @@
-import redis
-r = redis.Redis(host='localhost', port=6379, db=0)
-
-key = 'match:SCORE2:score'  # Use the actual key from your test
-value = r.get(key)
-if value:
-    print(f"Found Redis value: {value.decode('utf-8')}")
-else:
-    print("Key not found.")
